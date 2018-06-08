7f6fcaa130e45af186e78a8591f9b462c02fc094
Jose <jjosecamaluque@gmail.com>
2018-06-08 15:38:34 -0500

    Archivos

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarruselComponent } from './carrusel/carrusel.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarruselComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
